dfc0442305c8ffdfa91b250769ace964c4e42f81
Mikeal Rogers <mikeal.rogers@gmail.com>
2013-07-04 11:50:27 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,113 @@
+var util = require('util')
+  , events = require('events')
+  , peek = require('level-peek')
+  ;
+
+function parallel (arr, cb) {
+  var results = []
+    , complete = 0
+    ;
+  for (var i=0;i<arr.length;i++) {
+    ;(function (i) {
+      arr[i][0](function () {
+        results[i] = Array.prototype.slice.call(arguments)
+        complete += 1
+        if (complete === arr.length) {
+          for (var ii=0;ii<arr.length;ii++) {
+            arr[ii][1].apply(arr[ii][1], results[ii])
+          }
+          cb()
+        }
+      })
+    })(i)
+  }
+  if (arr.length === 0) cb()
+}
+
+function Mutex (lev) {
+  this.lev = lev
+  this.writing = false
+  this.reading = false
+  this.writes = []
+  this.reads = []
+}
+util.inherits(Mutex, events.EventEmitter)
+Mutex.prototype.put = function (key, value, cb) {
+  this.writes.push([{type:'put', key:key, value:value}, cb])
+  this.kick()
+}
+Mutex.prototype.get = function (key, cb) {
+  var self = this
+  this.reads.push([function (_cb) { self.lev.get(key, _cb) }, cb])
+  this.kick()
+}
+Mutex.prototype.del = function (key, cb) {
+  if (!cb) throw new Error('no cb')
+  this.writes.push([{type:'del', key:key}, cb])
+  this.kick()
+}
+Mutex.prototype.peekLast = function (opts, cb) {
+  var self = this
+  this.reads.push([function (_cb) { peek.last(self.lev, opts, _cb) }, cb])
+  this.kick()
+}
+Mutex.prototype.peekFirst = function (opts, cb) {
+  var self = this
+  this.reads.push([function (_cb) { peek.first(self.lev, opts, _cb) }, cb])
+  this.kick()
+}
+
+Mutex.prototype.kick = function () {
+  var self = this
+  if (this.writing || this.reading) return
+  if (!this.writes.length && !this.reads.length) return
+  if (!this._nt) {
+    this._nt = true
+    setImmediate(function () {
+      self._nt = false
+      self._read()
+    })
+  }
+}
+Mutex.prototype._read = function () {
+  var self = this
+    , reads = this.reads
+    ;
+  this.reading = true
+  this.reads = []
+  this.emit('reads', reads)
+  parallel(reads, function () {
+    self.reading = false
+    self._write()
+  })
+}
+Mutex.prototype._write = function () {
+  if (!this.writes.length) return this.kick()
+  var self = this
+    , writes = this.writes.map(function (x) {return x[0]})
+    , callbacks = this.writes.map(function (x) {return x[1]})
+    ;
+  this.writing = true
+  this.writes = []
+
+  this.emit('writes', writes)
+  this.lev.batch(writes, function (e) {
+    self.emit('flushed')
+    callbacks.forEach(function (cb) {
+      cb(e)
+    })
+    self.writing = false
+
+    self.kick()
+  })
+}
+Mutex.prototype.afterWrite = function (cb) {
+  var self = this
+  // must wrap in setImmediate in case this is called from a write callback
+  setImmediate(function () {
+    if (self.writing || self.writes.length) self.once('flushed', cb)
+    else cb()
+  })
+}
+
+module.exports = function (lev) {return new Mutex(lev)}
---
+++ test.js
@@ -0,0 +1,56 @@
+var lev = require('levelup')
+  , mutex = require('./')
+  , cleanup = require('cleanup')
+  , rimraf = require('rimraf')
+  , assert = require('assert')
+  , ok = require('okdone')
+  , async = require('async')
+  ;
+
+var d = cleanup(function (error) {
+  rimraf.sync('./testdb')
+  if (!error) ok.done()
+  if (error) process.exit(1)
+  process.exit()
+})
+
+var m = mutex(lev('./testdb'))
+
+var documents = ['test1', 'test2', 'test3', 'test4', 'test5', 'test6']
+
+async.map(documents, m.get.bind(m), function (e) {
+  if (!e) throw new Error('Documents should not exist yet')
+  ok('does not exit')
+})
+
+var writing = false
+
+async.map(documents, function (key, cb) {writing = true; m.put(key, 1, cb)}, function (e, results) {
+  if (e) throw e
+  ok('write')
+})
+
+setImmediate(function () {
+  assert.ok(writing)
+  async.map(documents, m.get.bind(m), function (e, results) {
+    if (e) throw e
+    assert.equal(results.length, results.reduce(function (x,y) {return x+parseInt(y)}, 0))
+    ok('exists')
+
+    m.peekLast({end:'test3'}, function (err, key, value) {
+      if (err) throw err
+      assert.equal(key, 'test3')
+      assert.equal(value, 1)
+      ok('peekLast')
+      m.peekFirst({start:'test4'}, function (err, key, value) {
+        if (err) throw err
+        assert.equal(key, 'test4')
+        assert.equal(value, 1)
+        ok('peekFirst')
+        d.cleanup()
+      })
+    })
+  })
+})
+
+ok.expect(5)
